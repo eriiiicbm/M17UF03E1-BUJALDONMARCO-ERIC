a14cd5cd18323896f2b30adaf5727b2d8781ac77
eriiiicbm <eric.bujaldon.7e2@itb.cat>
2021-02-10 19:14:24 +0100

    First Commit

---
+++ M17UF03E2-Bujaldon-Eric/Assets/Script/CharacterScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    int puntos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        puntos = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+    }
+
+    private void OnTriggerEnter2D(Collider collision)
+    {
+        if (collision.CompareTag("Score"))
+        {
+            puntos++;
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.CompareTag("Death"))
+        {
+            Application.Quit();
+        }
+    }
+
+
+}
